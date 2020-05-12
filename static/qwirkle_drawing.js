ec4de7f31bd7541dd7b1315c700be1b54c15183f
Philipp Thölke <p.thoelke@gmx.de>
2020-05-12 21:27:48 +0200

    Draw multiple cursors

--- static/qwirkle_drawing.js
+++ static/qwirkle_drawing.js
@@ -4,18 +4,26 @@ canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
 
+var mouseLocations = new Map();
+
 var socket = io();
 socket.on("connection", function(msg) {
 	console.log(msg)
 });
 
-socket.on("mouse", function(msg) {
-	console.log(msg)
+function update() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
-	ctx.arc(msg.x, msg.y, 3, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.fillText(msg.username, msg.x + 5, msg.y - 5);
+	for(let entry of mouseLocations) {
+		ctx.beginPath();
+		ctx.arc(entry[1][0], entry[1][1], 3, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.fillText(entry[0], entry[1][0] + 5, entry[1][1] - 5);
+	}
+}
+
+socket.on("mouse", function(msg) {
+	mouseLocations.set(msg.username, [msg.x, msg.y]);
+	update();
 });
 
 function onMouseDown(ev) {
@@ -25,9 +33,9 @@ function onMouseUp(ev) {
 }
 
 function onMouseMove(ev) {
-	socket.emit("mousePosition", {"x": ev.clientX, "y": ev.clientY})
+	socket.emit("mousePosition", {"x": ev.clientX, "y": ev.clientY});
 }
 
-canvas.addEventListener("pointerdown", onMouseDown)
-canvas.addEventListener("pointerup", onMouseUp)
-canvas.addEventListener("pointermove", onMouseMove)
+canvas.addEventListener("pointerdown", onMouseDown);
+canvas.addEventListener("pointerup", onMouseUp);
+canvas.addEventListener("pointermove", onMouseMove);
