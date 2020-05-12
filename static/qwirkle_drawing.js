c5e348de0627f0d1a8c4491491dfc16f38107d87
Philipp Thölke <p.thoelke@gmx.de>
2020-05-12 20:26:58 +0200

    Add flask sockets for mouse location sharing

---
+++ static/qwirkle_drawing.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var ctx = canvas.getContext("2d");
+
+var socket = io();
+socket.on("connection", function(msg) {
+	console.log(msg)
+});
+
+socket.on("mouse", function(msg) {
+	console.log(msg)
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
+	ctx.arc(msg.x, msg.y, 3, 0, 2 * Math.PI);
+	ctx.fill();
+});
+
+function onMouseDown(ev) {
+}
+
+function onMouseUp(ev) {
+}
+
+function onMouseMove(ev) {
+	socket.emit("mousePosition", {"x": ev.clientX, "y": ev.clientY})
+}
+
+canvas.addEventListener("pointerdown", onMouseDown)
+canvas.addEventListener("pointerup", onMouseUp)
+canvas.addEventListener("pointermove", onMouseMove)
